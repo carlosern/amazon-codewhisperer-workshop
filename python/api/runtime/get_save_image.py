95b70ac7c6618e6eb42083abd0f4904fb8a89005
Carlos Rojas <crojas@myxsales.com>
2023-03-29 22:32:41 -0400

    Image download CodeWhisperer assisted

--- python/api/runtime/get_save_image.py
+++ python/api/runtime/get_save_image.py
@@ -8,10 +8,30 @@ s3_client = boto3.client("s3")
 S3_BUCKET = os.getenv('BUCKET_NAME')
 
 # 1.) Function to get a file from url
+# Function to get a file from url
+
+
+def get_file_from_url(url):
+    try:
+        response = requests.get(url)
+        return response.content
+    except botocore.exceptions.ClientError as e:
+        print(e)
+        return None
+    finally:
+        pass
+
 
 # 2.) Function to upload image to S3
-
-# <<Amazon CodeWhisperer generated code goes here>>
+def upload_to_s3(file_name, file_content):
+    try:
+        s3_client.put_object(file_name, file_content, S3_BUCKET)
+        return True
+    except botocore.exceptions.ClientError as e:
+        print(e)
+        return False
+    finally:
+        pass
 
 
 def handler(event, context):
@@ -19,6 +39,8 @@ def handler(event, context):
     name = event["queryStringParameters"]["name"]
 
     # pass the output of method #1 as input to method #2
+    file_content = get_file_from_url(url)
+    upload_to_s3(name, file_content)
 
     return {
         'statusCode': 200,
